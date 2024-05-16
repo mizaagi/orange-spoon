43916df0719b325ca768c568477fd4d3e28f0a6a
mizaagi <126592834+mizaagi@users.noreply.github.com>
2024-05-16 19:38:38 +0000

    5/16

--- app.py
+++ app.py
@@ -3,5 +3,9 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-def hello_world():
-    return render_template("index.html", title="Hello")
+def home_page():
+    return render_template("index.html", title="Home")
+
+@app.route("/playlists")
+def playlists():
+    return render_template("playlists.html", title="Playlists")
