05ff1e432c5d9182a9b41975a89ee6a405998658
mizaagi <126592834+mizaagi@users.noreply.github.com>
2024-05-17 19:36:16 +0000

    Co-authored-by: trumpyb <trumpyb@users.noreply.github.com> Co-authored-by: Owen White <O-white1@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,11 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from datetime import datetime
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods = ["GET","POST"])
 def home_page():
+    if request.method == "POST":
+        audioFile = request.form["audio"]
+        
     return render_template("index.html", title="Home")
 
 @app.route("/playlists")
 def playlists():
     return render_template("playlists.html", title="Playlists")
+
+def get_date():
+    return datetime.now()
+
+def list_playlists():
+    return ["Example Playlist 1", "Example Playlist 2", "Example Playlist 3"]
+
+@app.route("/new_playlist")
+def new_playlist():
+    return render_template("new_playlist.html",
+                            title="Playlists",
+                            get_date=get_date,
+                            list_playlists=list_playlists,
+                            playlists=list_playlists())
